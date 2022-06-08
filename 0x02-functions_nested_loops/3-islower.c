140ecbdf20c7679a5c690d45b16def6907002a99
Samiz <samzeky23@gmail.com>
2022-06-08 10:25:01 -0700

    commit message functions nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *
- *
- * Return: Always 0.
- */
+  *
+  * _islower - Short description, single line
+  * @c: contains value to be compared
+  * Return: Always 0.
+  */
 int _islower(int c)
 {
 if (c > 'a' && c < 'z')
