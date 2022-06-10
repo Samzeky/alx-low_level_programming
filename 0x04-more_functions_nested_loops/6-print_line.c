3e128ebe10f83146331c92e7e00cc2d93bb7598c
Samiz <samzeky23@gmail.com>
2022-06-10 13:50:12 -0700

    my commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,9 +14,9 @@ _putchar('\n');
 int i;
 
 for (i = 1; i <= n; i++)
-       {
-       _putchar('_');
-       }
-       _putchar('\n');
-       }
-       }
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
