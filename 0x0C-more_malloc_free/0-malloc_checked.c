5bd2f8b4df31981eea73d482ddbaa382e93ef9cf
Samiz <samzeky23@gmail.com>
2022-07-01 17:33:14 -0700

    my commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,10 +9,9 @@
  */
 void *malloc_checked(unsigned int b)
 {
-  char *p;
-
-  p = malloc(b);
-  if (p == NULL)
-    exit(98);
-  return (p);
+char *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
 }
